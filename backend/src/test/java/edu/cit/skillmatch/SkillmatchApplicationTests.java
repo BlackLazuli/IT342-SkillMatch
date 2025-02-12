e01c6dbb63c4a909e43319fde61864e6d527d3e8
LAB14_USER ON WS28 <L14X08W28@citulab.net>
2025-02-12 09:06:26 +0800

    Added backend folder

---
+++ backend/src/test/java/edu/cit/skillmatch/SkillmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.skillmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

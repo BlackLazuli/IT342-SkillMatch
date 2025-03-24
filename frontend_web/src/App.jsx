0ae5bdd8b5a6cede61ccecb3ae6e35100b18b922
KDazai <feerdeellaban556@gmail.com>
2025-03-25 02:24:55 +0800

    Added Sign-Up Page

--- frontend_web/src/App.jsx
+++ frontend_web/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login/login';
-
+import SignUp from "./pages/sign-up/SignUp";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
   );

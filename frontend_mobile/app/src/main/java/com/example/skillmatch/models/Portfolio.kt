d54cb7a439abacbafa18ea84cf5b7cf6bf7843ff
KDazai <feerdeellaban556@gmail.com>
2025-04-25 02:30:04 +0800

    deleted The Corrupted Frontend_mobile to create a new one

--- frontend_mobile/app/src/main/java/com/example/skillmatch/models/Portfolio.kt
+++
@@ -1,8 +0,0 @@
-package com.example.skillmatch.models
-
-data class Portfolio(
-    val id: Long? = null,
-    val workExperience: String? = null,
-    val servicesOffered: List<Service>? = null,
-    val clientTestimonials: String? = null
-)

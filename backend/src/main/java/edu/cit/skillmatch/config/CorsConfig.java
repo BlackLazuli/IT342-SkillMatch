a001ba4cc78161edd92496c0769f7e33b9bffab6
KDazai <feerdeellaban556@gmail.com>
2025-04-29 01:07:13 +0800

    Update CorsConfig.java

--- backend/src/main/java/edu/cit/skillmatch/config/CorsConfig.java
+++ backend/src/main/java/edu/cit/skillmatch/config/CorsConfig.java
@@ -18,7 +18,9 @@ public class CorsConfig {
                         .allowedOrigins(
                             "http://localhost:5173", // Allow React frontend
                             "http://10.0.2.2:8080",  // Allow Android emulator
-                            "http://localhost:8080"  // Allow local testing
+                            "http://localhost:8080",
+                            "http://ec2-3-107-23-86.ap-southeast-2.compute.amazonaws.com:8080", // Add EC2 URL
+                            "http://ec2-3-107-23-86.ap-southeast-2.compute.amazonaws.com"      // Allow local testing
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
